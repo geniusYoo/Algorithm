2ea0d095c552830ee7a9c1d800a85c654a25476d
Youngjae You <99001085+geniusYoo@users.noreply.github.com>
2024-09-20 19:50:33 +0900

    [level 2] Title: 타겟 넘버, Time: 4.10 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        return dfs(numbers, 0, 0, target);
+    }
+
+    public int dfs(int[] numbers, int idx, int sum, int target) {
+        if (idx == numbers.length) {
+            return sum == target ? 1 : 0;
+        }
+        return dfs(numbers, idx + 1, sum + numbers[idx], target) +
+                dfs(numbers, idx + 1, sum - numbers[idx], target);
+    }
+}
