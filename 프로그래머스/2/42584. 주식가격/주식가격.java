a30ab8d65acde93fd3e46906289d9424a8b85712
Youngjae You <99001085+geniusYoo@users.noreply.github.com>
2025-02-07 22:11:47 +0900

    [level 2] Title: 주식가격, Time: 33.21 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,23 +4,22 @@ class Solution {
     public int[] solution(int[] prices) {
         int n = prices.length;
         int[] answer = new int[n];
-        // 스택의 값은 prices의 인덱스
+        
         Stack<Integer> stack = new Stack<>();
-        stack.push(0);
+        stack.push(0); // 초기 인덱스
         
         for (int i=1; i<prices.length; i++) {
-            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                int k = stack.pop();
-                answer[k] = i-k;
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int popped = stack.pop();
+                answer[popped] = i - popped;
             }
             stack.push(i);
         }
         
         while (!stack.isEmpty()) {
-            int k = stack.pop();
-            answer[k] = n - k - 1;
+            int poppedIdx = stack.pop();
+            answer[poppedIdx] = n - poppedIdx - 1;
         }
-        
         return answer;
     }
 }
