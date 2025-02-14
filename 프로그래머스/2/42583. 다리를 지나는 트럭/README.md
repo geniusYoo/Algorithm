c61f8280a7609d3e97cc6b453cf2471bedc34f24
Youngjae You <99001085+geniusYoo@users.noreply.github.com>
2025-02-14 21:24:47 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 33.25 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 95.1 MB, 시간: 25.65 ms
+메모리: 103 MB, 시간: 33.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 12일 21:53:38
+2025년 02월 14일 21:24:44
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,35 +1,51 @@
 import java.util.*;
-
+class Truck {
+    int weight;
+    int location;
+    public Truck (int weight) {
+        this.weight = weight;
+        this.location = 1;
+    }
+    public void move() {
+        this.location += 1;
+    }
+}
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
-        int time = 0;
-        int sum = 0;
-        for (int truck : truck_weights) {
-            while (true) {
-                if (deque.isEmpty()) {
-                    deque.addLast(truck);
-                    sum += truck;
-                    time++;
-                    break;
-                }
-                else if (deque.size() == bridge_length) {
-                    sum -= deque.pollFirst();
-                } else {
-                    if (sum + truck <= weight) {
-                        deque.addLast(truck);
-                        sum += truck;
-                        time++;
-                        break;
-                    }
-                    else {
-                        deque.addLast(0);
-                        time++;
-                    }
-
-                }
+        ArrayDeque<Truck> waitQ = new ArrayDeque<>();
+        ArrayDeque<Truck> moveQ = new ArrayDeque<>();
+        
+        for (int t : truck_weights) waitQ.addLast(new Truck(t));
+        
+        int curWeight = 0;
+        int answer = 1;
+        
+        while (!waitQ.isEmpty() || !moveQ.isEmpty()) {
+            answer++;
+            
+            // 다리가 비어있으면 트럭 올리기
+            if (!waitQ.isEmpty() && moveQ.isEmpty()) {
+                Truck curT = waitQ.pollFirst();
+                moveQ.addLast(curT);
+                curWeight += curT.weight;
+            }
+            
+            // 한칸씩 이동시키기
+            for (Truck t : moveQ) t.move();
+            
+            // 이동시켰을 때, 다리를 건넌 트럭은 다리에서 내리기
+            if (!moveQ.isEmpty() && moveQ.peek().location > bridge_length) {
+                Truck curT = moveQ.pollFirst();
+                curWeight -= curT.weight;
+            }
+            
+            // 다리에서 내린 후에 올릴 수 있는 트럭이 있다면 다리로 올리기
+            if (!waitQ.isEmpty() && curWeight + waitQ.peek().weight <= weight) {
+                Truck curT = waitQ.pollFirst();
+                moveQ.addLast(curT);
+                curWeight += curT.weight;
             }
         }
-        return time + bridge_length;
+        return answer;
     }
 }
