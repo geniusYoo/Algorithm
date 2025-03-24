00c3a04ae921c4eae458e78b0b5ca2e5794757a3
Young Jae You <99001085+geniusYoo@users.noreply.github.com>
2025-03-24 17:27:44 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.34 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,38 +1,35 @@
-import java.util.ArrayDeque;
+import java.util.*;
 class Node {
-    int r,c;
-
+    int r;
+    int c;
+    
     public Node(int r, int c) {
         this.r = r;
         this.c = c;
     }
 }
-
 class Solution {
-    public static int[] rx = {0, 0, 1, -1};
-    public static int[] ry = {1, -1, 0, 0};
+    
     public int solution(int[][] maps) {
-        int answer = 0;
-
         boolean[][] visited = new boolean[maps.length][maps[0].length];
         int[][] dist = new int[maps.length][maps[0].length];
+        int[] nx = {-1, 1, 0, 0};
+        int[] ny = {0, 0, -1, 1};
         
-        ArrayDeque<Node> deque = new ArrayDeque<>();
-        deque.addLast(new Node(0, 0));
         visited[0][0] = true;
         dist[0][0] = 1;
-
+        
+        ArrayDeque<Node> deque = new ArrayDeque<>();
+        deque.addLast(new Node(0,0));
         while (!deque.isEmpty()) {
             Node now = deque.pollFirst();
-
-            for (int i = 0; i < 4; i++) {
-                int nr = now.r + rx[i];
-                int nc = now.c + ry[i];
-
-                if (nr < 0 || nc < 0 || nr >= maps.length || nc >= maps[0].length)
-                    continue;
-                if (maps[nr][nc] == 0)
-                    continue;
+            
+            for (int i=0; i<4; i++) {
+                int nr = now.r + nx[i];
+                int nc = now.c + ny[i];
+                
+                if (nr >= maps.length || nc >= maps[0].length || nr < 0 || nc < 0) continue;
+                if (maps[nr][nc] == 0) continue;
                 
                 if (!visited[nr][nc]) {
                     visited[nr][nc] = true;
@@ -41,8 +38,7 @@ class Solution {
                 }
             }
         }
-
-
+        
         return dist[maps.length-1][maps[0].length-1] == 0 ? -1 : dist[maps.length-1][maps[0].length-1];
     }
 }
