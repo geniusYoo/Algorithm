0740b39047f1bdf378847f922e52f2f9bbec977a
Young Jae You <99001085+geniusYoo@users.noreply.github.com>
2025-05-22 00:54:54 +0900

    [level 2] Title: 타겟 넘버, Time: 4.58 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,13 +1,18 @@
 class Solution {
+    static int answer = 0;
     public int solution(int[] numbers, int target) {
-        return dfs(numbers, 0, 0, target);
+        dfs(numbers, 0, 0, target);
+        return answer;
     }
-
-    public int dfs(int[] numbers, int idx, int sum, int target) {
+    
+    public void dfs(int[] numbers, int idx, int sum, int target) {
         if (idx == numbers.length) {
-            return sum == target ? 1 : 0;
+            if (sum == target) {
+                answer++;
+            }
+            return;
         }
-        return dfs(numbers, idx + 1, sum + numbers[idx], target) +
-                dfs(numbers, idx + 1, sum - numbers[idx], target);
+        dfs(numbers, idx + 1, sum + numbers[idx], target);
+        dfs(numbers, idx + 1, sum - numbers[idx], target);
     }
 }
